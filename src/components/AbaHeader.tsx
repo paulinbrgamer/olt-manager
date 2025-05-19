c953f11dee90916e4b0de660094e28b294a6cb6e
Paulo <pauloferadosgames@gmail.com>
2025-05-19 09:00:38 -0300

    Mudnaça de opacidade no hover

--- src/components/AbaHeader.tsx
+++ src/components/AbaHeader.tsx
@@ -5,59 +5,59 @@ import { X } from 'lucide-react'
 import { type ponRequest } from '@/interfaces/request'
 import { useAbas } from '@/context/olt-abas-provider'
 interface Props {
-    abaInfo: abaInterface,
+  abaInfo: abaInterface,
 }
 function isPonRequest(obj: any): obj is ponRequest {
-    return obj && typeof obj.pon === 'number' && typeof obj.slot === 'number';
+  return obj && typeof obj.pon === 'number' && typeof obj.slot === 'number';
 }
 
 const AbaHeader: React.FC<Props> = ({ abaInfo }) => {
-    const { removeAba, abaslist, setcurrentAbaInfo, currentAbaInfo } = useAbas()
-    const handleDelete = (e: React.MouseEvent) => {
-        e.stopPropagation();
-        //variaveis para mudar automaticamente a aba ao se deletar, caso seja preciso
-        const currentIndex = abaslist.findIndex(aba => aba.id === abaInfo.id);
-        const isCurrent = currentAbaInfo === abaInfo.id;
-        const hasMultipleAbas = abaslist.length > 1;
-        const hasPreviousAba = currentIndex > 0;
-        if (hasMultipleAbas && isCurrent && hasPreviousAba) {
-            const previousAbaId = abaslist[currentIndex - 1];
-            setcurrentAbaInfo(previousAbaId.id);
-            console.log("Selecionando aba anterior:", previousAbaId);
-        }
+  const { removeAba, abaslist, setcurrentAbaInfo, currentAbaInfo } = useAbas()
+  const handleDelete = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    //variaveis para mudar automaticamente a aba ao se deletar, caso seja preciso
+    const currentIndex = abaslist.findIndex(aba => aba.id === abaInfo.id);
+    const isCurrent = currentAbaInfo === abaInfo.id;
+    const hasMultipleAbas = abaslist.length > 1;
+    const hasPreviousAba = currentIndex > 0;
+    if (hasMultipleAbas && isCurrent && hasPreviousAba) {
+      const previousAbaId = abaslist[currentIndex - 1];
+      setcurrentAbaInfo(previousAbaId.id);
+      console.log("Selecionando aba anterior:", previousAbaId);
+    }
 
-        removeAba(abaInfo.id);
-    };
+    removeAba(abaInfo.id);
+  };
 
-    return (
-        <div
-  onClick={() => setcurrentAbaInfo(abaInfo.id)}
-  className={`flex items-center gap-2 px-2 cursor-pointer 
-              flex-1 min-w-0 max-w-[200px] shrink 
+  return (
+    <div
+      onClick={() => setcurrentAbaInfo(abaInfo.id)}
+      className={`flex items-center gap-2 px-2 cursor-pointer 
+              flex-1 min-w-0 max-w-[200px] shrink hover:opacity-70 
               ${currentAbaInfo === abaInfo.id
-                ? "bg-background border-b-2 border-sky-600"
-                : "bg-accent border-border"}`}
->
-  {/* Container de informações com truncamento */}
-  <div className="flex flex-col flex-1 min-w-0 overflow-hidden">
-    <p className="text-sm font-semibold text-slate-400 truncate w-full">
-      {abaInfo.request?.olt.model + " " + abaInfo.request?.olt.location}
-    </p>
+          ? "bg-background border-b-2 border-sky-600"
+          : "bg-accent border-border"}`}
+    >
+      {/* Container de informações com truncamento */}
+      <div className="flex flex-col flex-1 min-w-0 overflow-hidden">
+        <p className="text-sm font-semibold text-slate-400 truncate w-full">
+          {abaInfo.request?.olt.model + " " + abaInfo.request?.olt.location}
+        </p>
 
-    {isPonRequest(abaInfo.request) && (
-      <p className="text-xs text-muted-foreground truncate w-full">
-        {"Pon : "+abaInfo.request.slot + "/" + abaInfo.request.pon}
-      </p>
-    )}
-  </div>
+        {isPonRequest(abaInfo.request) && (
+          <p className="text-xs text-muted-foreground truncate w-full">
+            {"Pon : " + abaInfo.request.slot + "/" + abaInfo.request.pon}
+          </p>
+        )}
+      </div>
 
-  {/* Botão de fechar sempre visível */}
-  <Button onClick={handleDelete} variant="ghost" size="sm" className="shrink-0">
-    <X className="text-red-500 h-4 w-4" />
-  </Button>
-</div>
+      {/* Botão de fechar sempre visível */}
+      <Button onClick={handleDelete} variant="ghost" size="sm" className="shrink-0">
+        <X className="text-red-500 h-4 w-4" />
+      </Button>
+    </div>
 
-    )
+  )
 }
 
 export default AbaHeader
