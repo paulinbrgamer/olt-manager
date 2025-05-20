60c71aceec4775d7e45e7eb8f33c5dcdbcaecd63
Paulo <pauloferadosgames@gmail.com>
2025-05-20 08:33:57 -0300

    Refatoração do OLTdashboard e comentarios

--- src/components/OltDashboard.tsx
+++ src/components/OltDashboard.tsx
@@ -21,12 +21,11 @@ interface Props {
 }
 const OltDashboard: React.FC<Props> = ({ abaInfoId }) => {
     const [modalSerial, setmodalSerial] = useState<boolean>(false) //state para modal
-    const [requestSerialInput, setrequestSerialInput] = useState<string>('')
-    const [requestPonInput, setrequestPonInput] = useState<{ slot: number | undefined, pon: number | undefined }>({ slot: undefined, pon: undefined })
-    const [modalPon, setmodalPon] = useState<boolean>(false)
+    const [requestSerialInput, setrequestSerialInput] = useState<string>('') //state para pegar o input do serial do modal
+    const [requestPonInput, setrequestPonInput] = useState<{ slot: number | undefined, pon: number | undefined }>({ slot: undefined, pon: undefined }) //state para pegar slot e pon
+    const [modalPon, setmodalPon] = useState<boolean>(false)//state para controlar o modal da pon 
     const { data, loading, fetchData, error } = useLazyFetch() // fetch hook
     const { updateAba, abaslist } = useAbas() //context api
-
     const [abaInfo, setAbaInfo] = useState<abaInterface>(getAbaFromList(abaInfoId!, abaslist))//inicializando a informação de aba usando a função acima
 
     const handleClickSerialOnu = async () => {
@@ -57,9 +56,11 @@ const OltDashboard: React.FC<Props> = ({ abaInfoId }) => {
             toast('Slot ou Pon invalido!!')
         }
     }
-
+    //useEffect para o response do fetch
     useEffect(() => {
+        //interface para mensagem de erro
         interface ErrorResponse { message: string }
+        //type guard 
         function isErrorResponse(data: responseData): data is ErrorResponse { return !Array.isArray(data); } //guarde que retorna se encontoru onu ou Não
         type responseData = OnuInfo[] | ErrorResponse
         //verificação se não consta erro e foi feito fetch
@@ -73,7 +74,6 @@ const OltDashboard: React.FC<Props> = ({ abaInfoId }) => {
                 //@ts-expect-error
                 const onulist: OnuInfo[] = data
                 const requestInfo: ponRequest = { olt: abaInfo.request?.olt!, slot: Number(onulist[0].slot), pon: Number(onulist[0]!.pon) }
-
                 updateAba({ ...abaInfo!, OnuList: onulist, request: requestInfo }) //atualizando aba atual com OnUlIST
                 toast("Busca realizada com sucesso!!", {
                     description: 'Onus carregadas na tabela...',
@@ -84,6 +84,7 @@ const OltDashboard: React.FC<Props> = ({ abaInfoId }) => {
 
         }
     }, [data])
+    //useEffect para erros no fetch
     useEffect(() => {
         if (error) {
             toast("Erro inesperado", {
@@ -91,110 +92,101 @@ const OltDashboard: React.FC<Props> = ({ abaInfoId }) => {
             })
         }
     }, [error])
+
+    //useEffect para atualizar o state abaInfo com o da lista do contexto
     useEffect(() => {
         setAbaInfo(getAbaFromList(abaInfoId!, abaslist)) //atualizando a info da aba local
     }, [abaslist])
 
     return (
-        <>
-            {abaInfo ?
-                <>
-                    <main className="col-end-3 flex-1 my-14 px-14 flex flex-col gap-8 max-w-full h-[600px] ">
-                        <div className='grid grid-cols-[0.5fr_0.5fr_0.5fr_1fr] gap-2 justify-center w-fit justify-items-end self-start ml-auto'>
-                            <IconButton className='w-fit self-end' variant={'link'} Icon={<TriangleAlert />} text='Incidentes' />
-                            <IconButton className='w-fit' variant={'outline'} Icon={<RotateCcw />} text='Atualizar' />
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <Button className='w-fit' variant={'outline'}>
-                                        <p>Carregar Pon</p>
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent className='w-40 border rounded-sm bg-background z-10 data-[state=open]:animate-in data-[state=open]:fade-in-40 data-[state=open]:slide-in-from-top-2 data-[state=closed]:animate-out data-[state=closed]:fade-out-40 data-[state=closed]:slide-out-to-top-2 '>
-                                    <DropdownMenuGroup>
-                                        <DropdownMenuItem onSelect={() => setmodalPon(true)}>
-                                            <Button className='w-full text-start' variant={'ghost'}>Buscar por Pon</Button>
-                                        </DropdownMenuItem>
-                                        <DropdownMenuItem onSelect={() => setmodalSerial(true)}>
-                                            <Button className='w-full text-start' variant={'ghost'}>Buscar por Serial</Button>
-                                        </DropdownMenuItem>
-                                    </DropdownMenuGroup>
+            <main className="col-end-3 flex-1 my-14 px-14 flex flex-col gap-8 max-w-full h-[600px] ">
+                <div className='grid grid-cols-[0.5fr_0.5fr_0.5fr_1fr] gap-2 justify-center w-fit justify-items-end self-start ml-auto'>
+                    <IconButton className='w-fit self-end' variant={'link'} Icon={<TriangleAlert />} text='Incidentes' />
+                    <IconButton className='w-fit' variant={'outline'} Icon={<RotateCcw />} text='Atualizar' />
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button className='w-fit' variant={'outline'}>
+                                <p>Carregar Pon</p>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className='w-40 border rounded-sm bg-background z-10 data-[state=open]:animate-in data-[state=open]:fade-in-40 data-[state=open]:slide-in-from-top-2 data-[state=closed]:animate-out data-[state=closed]:fade-out-40 data-[state=closed]:slide-out-to-top-2 '>
+                            <DropdownMenuGroup>
+                                <DropdownMenuItem onSelect={() => setmodalPon(true)}>
+                                    <Button className='w-full text-start' variant={'ghost'}>Buscar por Pon</Button>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem onSelect={() => setmodalSerial(true)}>
+                                    <Button className='w-full text-start' variant={'ghost'}>Buscar por Serial</Button>
+                                </DropdownMenuItem>
+                            </DropdownMenuGroup>
 
-                                </DropdownMenuContent>
+                        </DropdownMenuContent>
 
-                            </DropdownMenu>
-                            <SearchInput placeholder='Procurar Onu...' />
-                        </div>
-                        <Dialog open={modalSerial} onOpenChange={setmodalSerial}>
-                            <Overlay className="fixed inset-0 bg-black/50 backdrop-blur-xx z-50" />
-                            <DialogContent className="z-50 absolute bg-sidebar border self-center mt-[10%] mr-[15%]  px-5 pb-5 pt-2 rounded-md flex flex-col data-[state=open]:animate-in data-[state=open]:fade-in-40 data-[state=open]:slide-in-from-bottom-2
+                    </DropdownMenu>
+                    <SearchInput placeholder='Procurar Onu...' />
+                </div>
+                <Dialog open={modalSerial} onOpenChange={setmodalSerial}>
+                    <Overlay className="fixed inset-0 bg-black/50 backdrop-blur-xx z-50" />
+                    <DialogContent className="z-50 absolute bg-sidebar border self-center mt-[10%] mr-[15%]  px-5 pb-5 pt-2 rounded-md flex flex-col data-[state=open]:animate-in data-[state=open]:fade-in-40 data-[state=open]:slide-in-from-bottom-2
                           data-[state=closed]:animate-out data-[state=closed]:fade-out-40 data-[state=closed]:slide-out-to-bottom-2">
-                                <Button className='self-end w-8 ' size={'icon'} variant={'ghost'} onClick={() => setmodalSerial(false)}><X size={10} /></Button>
-                                <DialogTitle className="text-xl font-bold">Buscar Pon por Serial de uma Onu</DialogTitle>
-                                <DialogDescription className="text-sm text-muted-foreground">
-                                    Digite o serial da ONU
-                                </DialogDescription>
-                                <Input
-                                    value={requestSerialInput}
-                                    onChange={(e) => setrequestSerialInput(e.target.value)}
-                                    type="text"
-                                    className="mt-4 w-full "
-                                    placeholder="Ex: ZTEG12345678"
-                                />
-                                <LoaderButton className='mt-4' isLoading={loading} onClick={handleClickSerialOnu} variant='outline' text='Buscar' />
-                            </DialogContent>
-                        </Dialog>
-                        <Dialog open={modalPon} onOpenChange={setmodalPon}>
-                            <Overlay className="fixed inset-0 bg-black/50 backdrop-blur-xx z-50" />
-                            <DialogContent className="z-50 absolute bg-sidebar border self-center mt-[10%] mr-[15%]  px-5 pb-5 pt-2 rounded-md flex flex-col data-[state=open]:animate-in data-[state=open]:fade-in-40 data-[state=open]:slide-in-from-bottom-2
+                        <Button className='self-end w-8 ' size={'icon'} variant={'ghost'} onClick={() => setmodalSerial(false)}><X size={10} /></Button>
+                        <DialogTitle className="text-xl font-bold">Buscar Pon por Serial de uma Onu</DialogTitle>
+                        <DialogDescription className="text-sm text-muted-foreground">
+                            Digite o serial da ONU
+                        </DialogDescription>
+                        <Input
+                            value={requestSerialInput}
+                            onChange={(e) => setrequestSerialInput(e.target.value)}
+                            type="text"
+                            className="mt-4 w-full "
+                            placeholder="Ex: ZTEG12345678"
+                        />
+                        <LoaderButton className='mt-4' isLoading={loading} onClick={handleClickSerialOnu} variant='outline' text='Buscar' />
+                    </DialogContent>
+                </Dialog>
+                <Dialog open={modalPon} onOpenChange={setmodalPon}>
+                    <Overlay className="fixed inset-0 bg-black/50 backdrop-blur-xx z-50" />
+                    <DialogContent className="z-50 absolute bg-sidebar border self-center mt-[10%] mr-[15%]  px-5 pb-5 pt-2 rounded-md flex flex-col data-[state=open]:animate-in data-[state=open]:fade-in-40 data-[state=open]:slide-in-from-bottom-2
                           data-[state=closed]:animate-out data-[state=closed]:fade-out-40 data-[state=closed]:slide-out-to-bottom-2">
-                                <Button className='self-end w-8 ' size={'icon'} variant={'ghost'} onClick={() => setmodalPon(false)}><X size={10} /></Button>
-                                <DialogTitle className="text-xl font-bold">Carregar informações de Pon</DialogTitle>
-                                <DialogDescription className="text-sm text-muted-foreground">
-                                    Digite o slot e pon
-                                </DialogDescription>
-                                <Label className='mt-4' htmlFor={'slot'}>Slot</Label>
-                                <Input
-                                    id="slot"
-                                    type="number"
-                                    placeholder="Ex: 2"
-                                    className="mt-2 w-full"
-                                    value={requestPonInput.slot ?? ""}
-                                    onChange={(e) =>
-                                        setrequestPonInput((prev) => ({
-                                            ...prev,
-                                            slot: e.target.valueAsNumber,
-                                        }))
-                                    }
-                                />
+                        <Button className='self-end w-8 ' size={'icon'} variant={'ghost'} onClick={() => setmodalPon(false)}><X size={10} /></Button>
+                        <DialogTitle className="text-xl font-bold">Carregar informações de Pon</DialogTitle>
+                        <DialogDescription className="text-sm text-muted-foreground">
+                            Digite o slot e pon
+                        </DialogDescription>
+                        <Label className='mt-4' htmlFor={'slot'}>Slot</Label>
+                        <Input
+                            id="slot"
+                            type="number"
+                            placeholder="Ex: 2"
+                            className="mt-2 w-full"
+                            value={requestPonInput.slot ?? ""}
+                            onChange={(e) =>
+                                setrequestPonInput((prev) => ({
+                                    ...prev,
+                                    slot: e.target.valueAsNumber,
+                                }))
+                            }
+                        />
 
-                                <Label htmlFor="pon" className="mt-4 block">Pon</Label>
-                                <Input
-                                    id="pon"
-                                    type="number"
-                                    placeholder="Ex: 7"
-                                    className="mt-2 w-full"
-                                    value={requestPonInput.pon ?? ""}
-                                    onChange={(e) =>
-                                        setrequestPonInput((prev) => ({
-                                            ...prev,
-                                            pon: e.target.valueAsNumber,
-                                        }))
-                                    }
-                                />
+                        <Label htmlFor="pon" className="mt-4 block">Pon</Label>
+                        <Input
+                            id="pon"
+                            type="number"
+                            placeholder="Ex: 7"
+                            className="mt-2 w-full"
+                            value={requestPonInput.pon ?? ""}
+                            onChange={(e) =>
+                                setrequestPonInput((prev) => ({
+                                    ...prev,
+                                    pon: e.target.valueAsNumber,
+                                }))
+                            }
+                        />
 
-                                <LoaderButton className='mt-4' isLoading={loading} onClick={handleClickPonRequest} variant='outline' text='Buscar' />
-                            </DialogContent>
-                        </Dialog>
-
-                        <TableComponent onuList={abaInfo.OnuList} />
-                    </main>
-                </> : <div className='flex-1  col-end-3 flex flex-col justify-center items-center'>
-                    <p className='text-3xl font-medium'>Sem Tabs abertas...</p>
-                    <p className='text-lg'>👈 - Selecione uma Olt para abrir uma Tab</p>
-                    <p className='text-sm text-accent'>Version-0.5</p>
-                </div>}
-        </>
-
+                        <LoaderButton className='mt-4' isLoading={loading} onClick={handleClickPonRequest} variant='outline' text='Buscar' />
+                    </DialogContent>
+                </Dialog>
+                <TableComponent onuList={abaInfo.OnuList} />
+            </main>
     )
 }
 
