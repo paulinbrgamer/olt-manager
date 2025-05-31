01655f5b071b145717e204b2b36b0e2cfb97e68e
paulinbrgamer <pauloferadosgames@gmail.com>
2025-05-31 08:55:43 -0300

    Adição de teste para search Onu

--- src/tests/components/OltDashboard.test.tsx
+++ src/tests/components/OltDashboard.test.tsx
@@ -29,8 +29,8 @@ const Mockonu = [{
     fecActualMode: "disable",
     lastDown: "2025-05-17 15:08:06    2025-05-18 20:38:02 →  (DyingGasp)",
     signal: -22.292,
-  },
-  {
+},
+{
     slot: "1",
     pon: "3",
     id: "5",
@@ -49,62 +49,72 @@ const Mockonu = [{
     fecActualMode: "disable",
     lastDown: "2025-05-10 12:11:14    2025-05-14 08:06:23 →  (DyingGasp)",
     signal: -23.874,
-  }]
+}]
 beforeEach(() => {
     Object.defineProperty(HTMLElement.prototype, 'offsetHeight', {
-      configurable: true,
-      value: 700,
+        configurable: true,
+        value: 700,
     });
-  
+
     // mock do ResizeObserver, necessário para bibliotecas como react-virtual
     global.ResizeObserver = class {
-      observe() {}
-      unobserve() {}
-      disconnect() {}
+        observe() { }
+        unobserve() { }
+        disconnect() { }
     };
-  });
-  
-const MockRender = ({falseId} : {falseId: boolean})=>{
-    const {createAba,abaslist,updateAba} = useAbas()
+});
+
+const MockRender = ({ falseId }: { falseId: boolean }) => {
+    const { createAba, abaslist, updateAba } = useAbas()
     const [infoId, setinfoId] = useState<string | null>(null)
     useLayoutEffect(() => {
-        setinfoId(createAba({id:1,location:"Castanhal",model:"ZTE"}))
-      }, [])
+        setinfoId(createAba({ id: 1, location: "Castanhal", model: "ZTE" }))
+    }, [])
     useEffect(() => {
 
-        const upatingaba : abaInterface = {...getAbaFromList(infoId!,abaslist)!,OnuList:Mockonu}
+        const upatingaba: abaInterface = { ...getAbaFromList(infoId!, abaslist)!, OnuList: Mockonu }
         updateAba(upatingaba)
     }, [infoId])
-    
-    if(infoId && !falseId){
-        return <OltDashboard abaInfoId={infoId}/>
-    }else if(falseId){
-        return <OltDashboard abaInfoId={'invalido'}/>
+    useEffect(() => {
+        console.log(abaslist[0]);
+
+    }, [abaslist[0]])
+
+    if (infoId && !falseId) {
+        return <OltDashboard abaInfoId={infoId} />
+    } else if (falseId) {
+        return <OltDashboard abaInfoId={'invalido'} />
     }
-} 
-const renderDash = (falseId : boolean)=>{
+}
+const renderDash = (falseId: boolean) => {
     render(
         <AbasProvider>
-            <MockRender falseId={falseId}/>
+            <MockRender falseId={falseId} />
         </AbasProvider>
     )
 }
-describe("OLT-DASHBOARD renderização",()=>{
-    
-    it('Deve renderizar Id de aba invalida caso não encontre o ID passado por Props',()=>{
+describe("OLT-DASHBOARD renderização basica", () => {
+
+    it('Deve renderizar Id de aba invalida caso não encontre o ID passado por Props', () => {
         renderDash(true)
         expect(screen.queryByText('Id invalido de aba')).toBeInTheDocument()
     })
-    it('Deve renderizar todas os components iniciais',()=>{
+    it('Deve renderizar todas os components iniciais', () => {
         renderDash(false)
         expect(screen.queryByLabelText('Incidents-btn')).toBeInTheDocument()
         expect(screen.queryByLabelText('load-Pon')).toBeInTheDocument()
         expect(screen.queryByLabelText('search-Onu')).toBeInTheDocument()
         expect(screen.queryByLabelText('table-Onus')).toBeInTheDocument()
-              
-                
+
+
     })
-    it('Deve renderizar todas os onus registradas na Aba',()=>{
+
+
+
+})
+
+describe("OLT-DASHBOARD Renderizar OnusList", () => {
+    it('Deve renderizar todas os onus registradas na Aba', () => {
         renderDash(false)
         expect(screen.queryByLabelText('Incidents-btn')).toBeInTheDocument()
         expect(screen.queryByLabelText('load-Pon')).toBeInTheDocument()
@@ -112,22 +122,26 @@ describe("OLT-DASHBOARD renderização",()=>{
         expect(screen.queryByLabelText('table-Onus')).toBeInTheDocument()
         const onuListRenderized = screen.queryAllByLabelText('row')
         expect(onuListRenderized.length).toBe(Mockonu.length)
-        onuListRenderized.forEach(onu=>{
+        onuListRenderized.forEach(onu => {
             expect(onu).toBeInTheDocument()
-        })          
-                
+        })
+
     })
-    it('Deve filtrar todas as ONUs registradas na aba pelo nome pesquisado na searchBar', async () => {
+    it('Deve exibir 0 Onus caso pesquisa não corresponda a nenhuma Onu', async () => {
         renderDash(false);
-        const searchBtn = screen.getByLabelText('search-Onu');
-        await userEvent.type(searchBtn, 'fddawdfgwdawdawd');
-        const onuListRenderized = await screen.findAllByLabelText('row');
-        console.log(onuListRenderized.length);
-        
-        
-        //expect(onuListRenderized.length).toBe(Mockonu.length - 1);
+        const searchInput = screen.getByLabelText('search-Onu');
       
+        // Digita o texto na barra de busca
+        await userEvent.type(searchInput, 'Invalid-Name');
+      
+        // Aguarda a lista atualizar após debounce + render
+        await waitFor(() => {
+          const rows = screen.queryAllByLabelText('row');
+          expect(rows.length).toBe(0); // ou o valor esperado com base no mock
+          
+        }, { timeout: 1000 });
+        console.log(prettyDOM(screen.queryByLabelText('table-onus')!));
+
       });
-      
       
 })
