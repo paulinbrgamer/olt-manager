aed89c5a410774f5842ce068fad09f03dc4eb0fa
Paulo <pauloferadosgames@gmail.com>
2025-06-10 09:07:27 -0300

    Melhoria na mensagem de no aba

--- src/pages/OltManager.tsx
+++ src/pages/OltManager.tsx
@@ -10,12 +10,13 @@ const OltManager = () => {
         <div aria-label="screen-olt" className='grid grid-cols-[auto_2fr] grid-rows-[30px_1fr]  w-full '>
             <OltSideBar/>
             <TabGroup/>
+            
             {/*Conteudo principal renderizado da tab atual*/}
             {currentAbaInfo && abaslist.length > 0 ? (
                 <PonScreen abaInfoId={currentAbaInfo} key={currentAbaInfo} />
             ) : (
-                <div className='flex-1 flex col-end-3 justify-center items-center'>
-                    <p>Sem abas abertas...</p>
+                <div className='flex-1 flex col-end-3 justify-center items-center flex-col'>
+                    <p>👈 Selecione uma Olt para abrir uma aba</p>
                 </div>
             )}
             <p className='col-start-2 text-end pr-5 pb-1'>Beta-1.0</p>
